a7318076215106d570d9cfe9946547f69ce72827
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-05-05 20:47:48 -0500

    Add _get__define_search_methods

--- Scripts/Modules/params.py
+++ Scripts/Modules/params.py
@@ -3,11 +3,23 @@ def get_params() -> dict:
         "path graphics": "Graphics",
         "path results": "Results",
         "function name": "wood",
-        "search algorithm": "barzilai",
-        "search name": "barzilai",
-        "max iterations": 1e10,
+        "method": "barzilai",
+        "BB type": 1,
+        "max iterations": 1e5,
         "c1": 1e-4,
         "c2": 0.9,
-        "tau": 1e-3,
+        "tau": 1e-6,
     }
+    params = _define_search_methods(params)
+    return params
+
+
+def _define_search_methods(params) -> dict:
+    methods = {
+        "barzilai": {"search algorithm": "barzilai",
+                     "search name": "barzilai"},
+        "steepest": {"search algorithm": "steepest",
+                     "search name": "bisection"},
+    }
+    params.update(methods[params["method"]])
     return params
