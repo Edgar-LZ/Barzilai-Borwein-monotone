fae46468deaaf490299104dfedf2cf6b789b2f10
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-05-07 01:13:02 -0500

    Add all results for quadratic

--- Scripts/run.py
+++ Scripts/run.py
@@ -3,6 +3,9 @@ from Modules.params import get_params
 
 params = get_params()
 problem = orchestra_problem()
-problem.init(params)
-problem.solve()
-problem.save_results("01.csv")
+for i in range(100):
+    filename = str(i).zfill(3)
+    filename = "{}.csv".format(filename)
+    problem.init(params)
+    problem.solve()
+    problem.save_results(filename)
