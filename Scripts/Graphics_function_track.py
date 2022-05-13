d95084fcb5b89cc4ec49f0d74a6be5e491e84924
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-05-12 19:01:34 -0500

    Add function track to quadratic equation

--- Scripts/Graphics_function_track.py
+++ Scripts/Graphics_function_track.py
@@ -11,78 +11,97 @@ from numpy import array
 
 
 def create_temporal_params(x: array) -> dict:
-    params = {"x": x}
+    params = {"x": x, "lambda": 10}
     return params
 
 
-methods = [
-    "steepest",
-    "barzilai",
-    "ANGM",
-    "ANGR1",
-    "ANGR2",
-]
+methods = {
+    "steepest": "#03071E",
+    "barzilai": "#6A040F",
+    "ANGM": "#D00000",
+    "ANGR1": "#E85D04",
+    "ANGR2": "#FAA307",
+}
+
+datasets = {"rosembrock": {"x ticks": range(0, 105, 5),
+                           "x lim": 100,
+                           "y1 lim": 20,
+                           "y2 lim": 100,
+                           },
+            "quadratic": {"x ticks": range(0, 22, 2),
+                          "x lim": 20,
+                          "y1 lim": 2,
+                          "y2 lim": 10,
+                          "lambda": 10,
+                          },
+            }
 
 params = get_params()
-params["function name"] = "rosembrock"
-fig, (ax1, ax2) = plt.subplots(2, 1,
-                               sharex=True,
-                               figsize=(16, 8))
-for method in methods:
-    params["method"] = method
-    params = _define_function_params(params)
-    function = functions_class(params)
-    algorithm = optimize_method_position_track(params)
-    folder = algorithm.create_folder_results()
-    filename = "{}.csv".format(params["search algorithm"])
+for function_name in datasets:
+    params["function name"] = function_name
+    dataset = datasets[params["function name"]]
+    params.update(dataset)
+    fig, (ax1, ax2) = plt.subplots(2, 1,
+                                   sharex=True,
+                                   figsize=(16, 8))
+    for method in methods:
+        params["method"] = method
+        color = methods[method]
+        params = _define_function_params(params)
+        function = functions_class(params)
+        algorithm = optimize_method_position_track(params)
+        folder = algorithm.create_folder_results()
+        filename = "{}.csv".format(params["search algorithm"])
+        filename = join(folder,
+                        filename)
+        data = read_csv(filename)
+        positions = list(zip(data["0"],
+                             data["1"]))
+        data["x"] = positions
+        data["x"] = data["x"].apply(lambda x: array(x))
+        data["function"] = data["x"].apply(
+            lambda x: function.f(create_temporal_params(x)))
+        data["gradient"] = data["x"].apply(
+            lambda x: norm(function.gradient(x, create_temporal_params(x))))
+        ax1.plot(data["function"],
+                 marker=".",
+                 ls="--",
+                 alpha=0.7,
+                 label=method,
+                 color=color)
+        ax1.grid(ls="--",
+                 color="#000000",
+                 alpha=0.4)
+        ax1.set_xlim(0, dataset["x lim"])
+        ax1.set_xticks([value
+                        for value in dataset["x ticks"]])
+        ax1.set_ylabel("$f(x)$",
+                       fontsize=13)
+        ax1.set_ylim(0, dataset["y1 lim"])
+        ax2.plot(data["gradient"],
+                 marker=".",
+                 ls="--",
+                 color=color,
+                 alpha=0.7)
+        ax2.grid(ls="--",
+                 color="#000000",
+                 alpha=0.4)
+        ax2.set_xlabel("Iteraciones",
+                       fontsize=13)
+        ax2.set_ylabel("$\\nabla f(x)$",
+                       fontsize=13)
+        ax2.set_ylim(0, dataset["y2 lim"])
+    fig.legend(ncol=5,
+               fontsize=13,
+               frameon=False,
+               loc="upper center")
+    plt.tight_layout(pad=2.5)
+    filename = "{}.png".format(params["function name"])
+    folder = join(params["path graphics"],
+                  "function")
+    makedirs(folder,
+             exist_ok=True)
     filename = join(folder,
                     filename)
-    data = read_csv(filename)
-    positions = list(zip(data["0"],
-                         data["1"]))
-    data["x"] = positions
-    data["x"] = data["x"].apply(lambda x: array(x))
-    data["function"] = data["x"].apply(
-        lambda x: function.f(create_temporal_params(x)))
-    data["gradient"] = data["x"].apply(
-        lambda x: norm(function.gradient(x, create_temporal_params(x))))
-    ax1.plot(data["function"],
-             marker=".",
-             ls="--",
-             alpha=0.7,
-             label=method)
-    ax1.grid(ls="--",
-             color="#000000",
-             alpha=0.4)
-    ax1.set_xlim(0, 100)
-    ax1.set_xticks([value
-                    for value in range(0, 105, 5)])
-    ax1.set_ylabel("$f(x)$",
-                   fontsize=13)
-    ax1.set_ylim(0, 40)
-    ax2.plot(data["gradient"],
-             marker=".",
-             ls="--",
-             alpha=0.7)
-    ax2.grid(ls="--",
-             color="#000000",
-             alpha=0.4)
-    ax2.set_xlabel("Iteraciones",
-                   fontsize=13)
-    ax2.set_ylabel("$\\nabla f(x)$",
-                   fontsize=13)
-    ax2.set_ylim(0, 200)
-fig.legend(ncol=5,
-           fontsize=13,
-           frameon=False,
-           loc="upper center")
-plt.tight_layout(pad=2.5)
-filename = "{}.png".format(params["function name"])
-folder = join(params["path graphics"],
-              "function")
-makedirs(folder,
-         exist_ok=True)
-filename = join(folder,
-                filename)
-plt.savefig(filename,
-            dpi=400)
+    plt.savefig(filename,
+                dpi=400)
