0b10896b5f33a9219720dd3725c95acddb10b7cb
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-05-25 22:25:30 -0500

    Change lw

--- Scripts/Graphics_function_track.py
+++ Scripts/Graphics_function_track.py
@@ -14,27 +14,28 @@ def create_temporal_params(x: array) -> dict:
     return params
 
 
-datasets = {"rosembrock": {"x ticks": range(0, 105, 5),
-                           "x lim": 100,
-                           "y1 lim": 20,
-                           "y2 lim": 100,
-                           },
-            "quadratic": {"x ticks": range(0, 22, 2),
-                          "x lim": 20,
-                          "y1 lim": 2,
-                          "y2 lim": 10,
-                          "lambda": 10,
-                          },
-            }
+datasets = {
+    "rosembrock": {
+        "x ticks": range(0, 105, 5),
+        "x lim": 100,
+        "y1 lim": 20,
+        "y2 lim": 100,
+    },
+    "quadratic": {
+        "x ticks": range(0, 22, 2),
+        "x lim": 20,
+        "y1 lim": 2,
+        "y2 lim": 10,
+        "lambda": 10,
+    },
+}
 methods = get_methods_names()
 params = get_params()
 for function_name in datasets:
     params["function name"] = function_name
     dataset = datasets[params["function name"]]
     params.update(dataset)
-    fig, (ax1, ax2) = plt.subplots(2, 1,
-                                   sharex=True,
-                                   figsize=(16, 8))
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(16, 8))
     for method in methods:
         params["method"] = method
         color = methods[method]["color"]
@@ -44,11 +45,9 @@ for function_name in datasets:
         algorithm = optimize_method_position_track(params)
         folder = algorithm.create_folder_results()
         filename = "{}.csv".format(params["search algorithm"])
-        filename = join(folder,
-                        filename)
+        filename = join(folder, filename)
         data = read_csv(filename)
-        positions = list(zip(data["0"],
-                             data["1"]))
+        positions = list(zip(data["0"], data["1"]))
         data["x"] = positions
         data["x"] = data["x"].apply(lambda x: array(x))
         data["function"] = data["x"].apply(
@@ -56,25 +55,28 @@ for function_name in datasets:
         data["gradient"] = data["x"].apply(
             lambda x: norm(function.gradient(x, create_temporal_params(x))))
         ax1.plot(data["function"],
-                 marker=".",
+                 marker="o",
                  ls="--",
                  alpha=0.7,
                  label=title,
-                 color=color)
+                 color=color,
+                 lw=3)
         ax1.grid(ls="--",
                  color="#000000",
                  alpha=0.4)
-        ax1.set_xlim(0, dataset["x lim"])
+        ax1.set_xlim(0,
+                     dataset["x lim"])
         ax1.set_xticks([value
                         for value in dataset["x ticks"]])
-        ax1.set_ylabel("$f(x)$",
-                       fontsize=13)
-        ax1.set_ylim(0, dataset["y1 lim"])
+        ax1.set_ylabel("$f(x)$", fontsize=13)
+        ax1.set_ylim(0,
+                     dataset["y1 lim"])
         ax2.plot(data["gradient"],
                  marker=".",
                  ls="--",
                  color=color,
-                 alpha=0.7)
+                 alpha=0.7,
+                 lw=3)
         ax2.grid(ls="--",
                  color="#000000",
                  alpha=0.4)
